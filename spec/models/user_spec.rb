a8b7b0b85b4c4208ddbc432edd66020a206965dc
joeseiden <joe.seiden214@gmail.com>
2017-05-05 17:30:23 -0700

    Initial rspec workshop commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:favorites) }
+    it { should have_many(:favorite_benches) }
+  end
+end
